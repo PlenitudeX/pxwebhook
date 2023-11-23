4e8fb2abe35fbfbf1441b5043534e39bba0531ba
Raymundo <raymundo@plenitudex.com>
2023-11-23 16:17:28 -0300

    zero

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use('/api/v1', require('./controlador/api_v1'));
 app.use('/api/v2', require('./controlador/api_v2'));
 
 app.get('/', function(req, res) {
-  res.send('Hello from root route.')
+  res.send('Hello from root route ta.')
 });
 
 /* istanbul ignore next */
