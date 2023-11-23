20dd7642ecc4669cb056cb3903efaf1c10bb6648
Raymundo <raymundo@plenitudex.com>
2023-11-23 16:16:09 -0300

    zero

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const express = require('express')
+var app = module.exports = express();
+
+app.use('/api/v1', require('./controlador/api_v1'));
+app.use('/api/v2', require('./controlador/api_v2'));
+
+app.get('/', function(req, res) {
+  res.send('Hello from root route.')
+});
+
+/* istanbul ignore next */
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Express started on port 3000');
+}
